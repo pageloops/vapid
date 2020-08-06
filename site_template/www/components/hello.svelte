cd685ca224a46a424b99d7c000a7bfd0621e5ec4
Gal Tashma <galtashma@gmail.com>
2020-08-06 21:28:27 +0300

    added components parts to site tempalte

---
+++ site_template/www/components/hello.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="my-element"/>
+<style>
+h1 {
+color: pink;
+}
+</style>
+<script>
+    let name = 'world';
+    setInterval(() => {
+      name = name + '.';   
+    }, 1000);
+</script>
+
+<h1>Hello {name}!</h1>
